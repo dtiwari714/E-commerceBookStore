2092821bf91dd0402f010bdc578c089af6b3c597
DURGESH TIWARI <77872399+dtiwari714@users.noreply.github.com>
2023-04-10 22:29:06 +0530

    Delete frontend directory

--- frontend/src/Store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import { ProductReducer } from './Reducers/ProductReducer';
-const store = configureStore({
-    reducer:{
-        products:ProductReducer
-    }
-});
-
-export default store;
